12668a45ea7bd6869a50064759e7c69ac9daa285
Hiroshi Takeda <takejaponpon+git@gmail.com>
2015-05-06 18:58:27 +0900

    Add a new solution Add a new project with Cloo

---
+++ opencl1/ConsoleApplication1/ClooClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cloo;
+
+namespace ConsoleApplication1
+{
+    class ClooClass
+    {
+        public void Init()
+        {
+            var platforms = ComputePlatform.Platforms;
+            foreach (var platform in platforms)
+            {
+                Console.WriteLine("Platform: {0} ({1})", platform.Name, platform.Version);
+                Console.WriteLine("Number of Devices: {0}", platform.Devices.Count);
+            }
+        }
+    }
+}
